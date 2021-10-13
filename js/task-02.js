07c12baec4de9ef91e67f7cb98e77b9016812371
dimatomilow <masikuspaime@gmail.com>
2021-10-13 21:18:57 +0300

    2 задание

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const elements = ingredients.map(ingr => {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingr;
+  liEl.classList.add("item");
+  return liEl;
+})
+
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+ingredientsEl.append(...elements);
+console.log(ingredientsEl);
