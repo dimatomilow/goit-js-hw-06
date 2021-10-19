7d8b73071faebf299a3bd06e7095873e9a3836ab
dimatomilow <masikuspaime@gmail.com>
2021-10-19 20:49:54 +0300

    Задача 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form")
+form.addEventListener("submit", onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (email === "" || password === "") {
+        return alert("Заполните все поля");
+    }
+
+    console.log("Email", email)
+    console.log("Password", password)
+
+     form.reset();
+}
 
