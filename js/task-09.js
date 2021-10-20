e853585d5d0f11aae1c5f67036810b0e253bc357
dimatomilow <masikuspaime@gmail.com>
2021-10-20 17:05:08 +0300

    Задача 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorValue = document.querySelector(".color");
+const body = document.body;
+const button = document.querySelector(".change-color");
+
+const randomColor = getRandomHexColor();
+
+button.addEventListener("click", changeColorClick);
+
+function changeColorClick() {
+  body.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+
+}
